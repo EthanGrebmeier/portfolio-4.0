9f0f7b077091e4ef76b349deb3c7c31d92429b5d
Ethan Grebmeier <ethan@assembleinc.com>
2024-10-23 13:05:10 -0700

    fix: max link text width

--- src/components/BlockLink.tsx
+++ src/components/BlockLink.tsx
@@ -41,7 +41,7 @@ const BubbleLink = ({
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0, transition: { duration: 0.1 } }}
-                className="relative"
+                className="relative w-max"
                 ref={textRef}
               >
                 {text}
